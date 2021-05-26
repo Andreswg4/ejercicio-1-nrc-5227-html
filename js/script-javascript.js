05d9bf9436492586816baf3a4568b86e63abcab2
Diego Velasco <divelasco@espe.edu.ec>
2021-05-26 11:30:47 -0500

    Ejemplo JS

--- js/script-javascript.js
+++ js/script-javascript.js
@@ -1,4 +1,6 @@
-function changeTitle(){
+/* Función que cambia el titulo de txtEjemplo y desaparece el botón  */
+function changeTitle(){    
     document.getElementById('txtEjemplo').innerText = 'Nuevo título';
     document.getElementById('btnClick').remove();
+    document.getElementById('txtTitulo').innerText = 'JavaScript';
 }
